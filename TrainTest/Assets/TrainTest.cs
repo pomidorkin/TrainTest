501be3187d2094f1084de29b32d9cfd35f1272e5
Pomidorkin <pbriuzgin@gmail.com>
2021-06-19 20:02:18 +0900

    Many bugs, but track switching functionality is added

---
+++ TrainTest/Assets/TrainTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainTest : MonoBehaviour
+{
+    [SerializeField]
+    Cinemachine.CinemachinePathBase pathTwo;
+
+    Cinemachine.CinemachinePathBase path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = gameObject.GetComponent<Cinemachine.CinemachineDollyCart>().m_Path;
+        // Получаем у объекта Train его path и назначаем ему другой path
+        gameObject.GetComponent<Cinemachine.CinemachineDollyCart>().m_Path = pathTwo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
